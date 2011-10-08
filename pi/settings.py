25d79e9369850ef513f26eca8e175a6e44c615a9
Oliver Zhou <oliver.zhoumi@gmail.com>
2011-10-08 15:31:53 +0800

    pseudo ui

--- pi/settings.py
+++ pi/settings.py
@@ -1,5 +1,8 @@
 # Django settings for pi project.
 
+import os
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "template/static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -106,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/luoj/projects/Django-1.3/pi/template',
+    os.path.join(PROJECT_ROOT, "template")
 )
 
 INSTALLED_APPS = (
